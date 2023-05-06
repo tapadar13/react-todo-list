35ebb9b316975a2e3971b6c2be35d462ffc85290
tapadar13 <monsurismyname@gmail.com>
2023-05-06 20:43:00 +0530

    all functionalities implemented

--- src/App.jsx
+++ src/App.jsx
@@ -52,6 +52,7 @@ function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map((todo) => {
           return (
             <li key={todo.id}>
