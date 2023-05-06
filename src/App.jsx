889fb525f233c89e5a6d5bd40da965fb73754e0f
tapadar13 <monsurismyname@gmail.com>
2023-05-06 13:30:18 +0530

    delete function implemented

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,24 @@ function App() {
     setNewItem("");
   };
 
+  const toggleTodo = (id, completed) => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -37,16 +55,20 @@ function App() {
         {todos.map((todo) => {
           return (
             <li key={todo.id}>
-              <label htmlFor="item">
+              <label>
                 <input
                   type="checkbox"
-                  id="item"
                   checked={todo.completed}
-                  onChange={toggleTodo(todo.id, e.target.checked)}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
                 />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                Delete
+              </button>
             </li>
           );
         })}
