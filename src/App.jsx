cdd1734eadcb14682c7e56c444f24251df9c23aa
tapadar13 <monsurismyname@gmail.com>
2023-05-06 09:27:54 +0530

    initial layout implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import "./styles.css";
+
 function App() {
-  return "Hi";
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="text">Add Item</label>
+          <input type="text" id="text" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 export default App;
